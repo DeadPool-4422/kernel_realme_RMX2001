3f989645751145b5a66b48ae503abbbfe4b03066
DeadPool-4422 <dynamicplayer444@gmail.com>
2024-01-09 12:46:24 +0530

    Cleared all old build packages and backed up the debian folder

---
+++ bak/debian/kernel-info.mk
@@ -0,0 +1,138 @@
+########################################################################
+# Kernel settings
+########################################################################
+
+# Kernel variant. This is currently used only on the Source package name.
+VARIANT = android
+
+# Kernel base version
+KERNEL_BASE_VERSION = 4.14.141
+
+# The kernel cmdline to use
+KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 buildvariant=userdebug droidian.lvm.prefer systemd.unified_cgroup_hierarchy=0
+
+# Slug for the device vendor. This is going to be used in the KERNELRELEASE
+# and package names.
+DEVICE_VENDOR = realme
+
+# Slug for the device model. Like above.
+DEVICE_MODEL = rmx2001
+
+# Slug for the device platform. If unsure, keep this commented.
+# DEVICE_PLATFORM = platform
+
+# Marketing-friendly full-name. This will be used inside package descriptions
+DEVICE_FULL_NAME = RealMe 6
+
+# Whether to use configuration fragments to augment the kernel configuration.
+# If unsure, keep this to 0.
+KERNEL_CONFIG_USE_FRAGMENTS = 0
+
+# Whether to use diffconfig to generate the device-specific configuration.
+# If you enable this, you should set KERNEL_CONFIG_USE_FRAGMENTS to 1.
+# If unsure, keep this to 0.
+KERNEL_CONFIG_USE_DIFFCONFIG = 0
+
+# Defconfig to use
+KERNEL_DEFCONFIG = RMX2001_defconfig
+
+# Whether to include DTBs with the image. Use 0 (no) or 1.
+KERNEL_IMAGE_WITH_DTB = 1
+
+# Path to the DTB
+KERNEL_IMAGE_DTB = arch/arm64/boot/dts/mediatek/mt6785.dtb
+
+# Whether to include a DTB Overlay. Use 0 (no) or 1.
+KERNEL_IMAGE_WITH_DTB_OVERLAY = 0
+
+# Various other settings that will be passed straight to mkbootimg
+KERNEL_BOOTIMAGE_PAGE_SIZE = 2048
+KERNEL_BOOTIMAGE_BASE_OFFSET = 0x40078000
+KERNEL_BOOTIMAGE_KERNEL_OFFSET = 0x00008000
+KERNEL_BOOTIMAGE_INITRAMFS_OFFSET = 0x07c08000
+KERNEL_BOOTIMAGE_SECONDIMAGE_OFFSET = 0xbff88000
+KERNEL_BOOTIMAGE_TAGS_OFFSET = 0x0bc08000
+KERNEL_BOOTIMAGE_DTB_OFFSET = 0x0bc08000
+
+# Kernel bootimage version. Defaults to 0 (legacy header).
+KERNEL_BOOTIMAGE_VERSION = 2
+
+########################################################################
+# Android verified boot
+########################################################################
+
+# Whether to build a flashable vbmeta.img. Please note that currently
+# only empty vbmeta images (disabling verified boot) can be generated.
+# Use 0 (no) or 1 (default).
+DEVICE_VBMETA_REQUIRED = 0
+
+# Samsung devices require a special flag. Enable the following if your
+# device is a Samsung device that requires flag 0 to be present
+# Use 0 (no, default) or 1.
+DEVICE_VBMETA_IS_SAMSUNG = 0
+
+########################################################################
+# Automatic flashing on package upgrades
+########################################################################
+
+# Whether to enable kernel upgrades on package upgrades. Use 0 (no) or 1.
+FLASH_ENABLED = 1
+
+# If your device is treble-ized, but aonly, you should set the following to
+# 1 (yes).
+FLASH_IS_AONLY = 0
+
+# Device manufacturer. This must match the `ro.product.vendor.manufacturer`
+# Android property. If you don't want to specify this, leave it undefined,
+# FLASH_INFO_CPU will be checked instead.
+FLASH_INFO_MANUFACTURER = realme
+
+# Device model. This must match the `ro.product.vendor.model`
+# Android property. If you don't want to specify this, leave it undefined,
+# FLASH_INFO_CPU will be checked instead.
+FLASH_INFO_MODEL = RMX2001
+
+# Device CPU. This will be grepped against /proc/cpuinfo to check if
+# we're running on the specific device. Note this is a last-resort
+# method, specifying FLASH_INFO_MANUFACTURER and FLASH_INFO_MODEL is
+# recommended.
+FLASH_INFO_CPU = MT6785
+
+########################################################################
+# Kernel build settings
+########################################################################
+
+# Whether to cross-build. Use 0 (no) or 1.
+BUILD_CROSS = 1
+
+# (Cross-build only) The build triplet to use. You'll probably want to
+# use aarch64-linux-android- if building Android kernels.
+BUILD_TRIPLET = aarch64-linux-android-
+
+# (Cross-build only) The build triplet to use with clang. You'll probably
+# want to use aarch64-linux-gnu- here.
+BUILD_CLANG_TRIPLET = aarch64-linux-gnu-
+
+# The compiler to use. Recent Android kernels are built with clang.
+BUILD_CC = clang
+
+# Extra paths to prepend to the PATH variable. You'll probably want
+# to specify the clang path here (the default).
+BUILD_PATH = /buildd/sources/clang/bin
+
+# Extra packages to add to the Build-Depends section. Mainline builds
+# can have this section empty, unless cross-building.
+DEB_TOOLCHAIN = linux-initramfs-halium-generic:arm64, binutils-aarch64-linux-gnu, clang-android-6.0-4691093, gcc-4.9-aarch64-linux-android, g++-4.9-aarch64-linux-android, libgcc-4.9-dev-aarch64-linux-android-cross
+
+# Where we're building on
+DEB_BUILD_ON = amd64
+
+# Where we're going to run this kernel on
+DEB_BUILD_FOR = arm64
+
+# Target kernel architecture
+KERNEL_ARCH = arm64
+
+# Kernel target to build
+KERNEL_BUILD_TARGET = Image.gz
+
--- debian/linux-headers-4.14.141-realme-rmx2001/usr/src/linux-headers-4.14.141-realme-rmx2001/include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "4.14.141-realme-rmx2001"
